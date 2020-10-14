16e1f7c11a3fe60e86e5d7c3d3944f274304d870
Piotr Glodek <pioglodek@gmail.com>
2020-10-14 10:32:21 +0200

    Create Home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Navigation } from './components/Navigation';
 import { Providers } from './Providers';
+import { Switch, Route } from 'react-router-dom';
+// components
+import { Navigation } from './components';
+// pages
+import { Home } from './pages';
 
 function App() {
   return (
     <Providers>
       <Navigation />
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
     </Providers>
   );
 }
