920f02e8fe43ec6a4bcf3b22a25c71915d7da12b
Piotr Glodek <pioglodek@gmail.com>
2020-10-14 13:25:41 +0200

    Rexport renamed components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+// NAVIGATION
 export { Navigation } from './Navigation/Navigation';
 export { Nav } from './Navigation/Nav';
 export { Logo } from './Navigation/Logo';
@@ -5,8 +6,12 @@ export { Menu } from './Navigation/Menu';
 export { NavLink } from './Navigation/NavLink';
 export { SearchIcon } from './Navigation/SearchIcon';
 export { Search } from './Navigation/Search';
-export { Card } from './Movie/Card';
+// Layout
 export { Container } from './Layout/Container';
 export { Section } from './Layout/Section';
 export { SectionTitle } from './Layout/SectionTitle';
-export { CardList } from './Movie/CardList';
+export { withGap } from './Layout/withGap';
+export { CardListSkeleton } from './Layout/CardListSkeleton';
+// Movie
+export { MovieCard } from './Movie/MovieCard';
+export { MovieCardList } from './Movie/MovieCardList';
