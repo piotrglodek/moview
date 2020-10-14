6c8bd66bd61a3c4789e523593128dc701dc9b710
Piotr Glodek <pioglodek@gmail.com>
2020-10-14 16:26:40 +0200

    Create fetch genre for serie

--- src/api/static.js
+++ src/api/static.js
@@ -1,6 +1,10 @@
 const base_url = `https://api.themoviedb.org/3/`;
 export const base_poster_url = `https://image.tmdb.org/t/p/w154`;
+
+// movie genres
 export const movie_genres_url = `${base_url}genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
+// serie genres
+export const serie_genres_url = `${base_url}genre/tv/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
 
 export const fetchTypes = {
   movies: [
