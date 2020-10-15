91dd74f3784bb074525c0572285b2e1c9ef7a595
Piotr Glodek <pioglodek@gmail.com>
2020-10-15 11:02:34 +0200

    Create search api method

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
 export { fetchTypes, base_poster_url, base_url } from './static';
 export { fetchAll } from './fetchAll';
 export { fetchGenres } from './fetchGenres';
+export { searchAll } from './searchAll';
