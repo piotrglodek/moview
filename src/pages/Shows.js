0f1afd6252afa61c1d37fdc1d3f1d23deb7c2743
Piotr Glodek <pioglodek@gmail.com>
2020-10-16 22:06:06 +0200

    Create Shows page

---
+++ src/pages/Shows.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+// components
+import { Container, CardList } from '../components';
+
+export const Shows = ({ mediaType }) => {
+  let { pageNumber } = useParams();
+  pageNumber = parseInt(pageNumber);
+  return (
+    <Container>
+      <CardList
+        wrap
+        jusitfy='center'
+        havePagination
+        mediaType={mediaType}
+        title={mediaType}
+        page={pageNumber}
+      />
+    </Container>
+  );
+};
+
+Shows.propTypes = {
+  mediaType: PropTypes.string.isRequired,
+};
