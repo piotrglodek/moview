c22a42f23ac5f2e783e7e7cbc9de03704c3e7839
Piotr Glodek <pioglodek@gmail.com>
2020-10-16 22:07:04 +0200

    Add new props for Shows page,

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -2,28 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 // // components
-import { Card, CardSkeleton } from '../';
+import { Card, CardSkeleton, Pagination } from '../';
 // api
 import { useQuery } from 'react-query';
 import { fetchSection } from '../../api';
 
-export const CardList = ({ wrap, title, url, mediaType }) => {
-  const { isLoading, isError, data, error, isFetching } = useQuery(
-    [title, url],
+export const CardList = ({
+  wrap,
+  jusitfy,
+  havePagination,
+  title,
+  url,
+  mediaType,
+  page,
+}) => {
+  const { isLoading, isError, data, error } = useQuery(
+    [title, url, page],
     fetchSection
   );
   return (
-    <StyledContainer wrap={wrap ? 'wrap' : 'nowrap'}>
-      {isLoading || isFetching ? (
-        <CardSkeleton n={20} />
+    <>
+      {isLoading ? (
+        <StyledContainer
+          wrap={wrap ? 'wrap' : 'nowrap'}
+          jusitfy={jusitfy ? jusitfy : 'flex-start'}
+        >
+          <CardSkeleton n={20} />
+        </StyledContainer>
       ) : isError ? (
         <p>Sorry, something went wrong. Couldn't fetch data.{error.message}</p>
       ) : (
-        data.results.map(result => (
-          <Card key={result.id} data={result} mediaType={mediaType} />
-        ))
+        <>
+          {havePagination && (
+            <Pagination
+              page={data.page}
+              maxPage={data.total_pages}
+              path={mediaType === 'movie' ? 'movies' : 'tv'}
+            />
+          )}
+          <StyledContainer
+            wrap={wrap ? 'wrap' : 'nowrap'}
+            jusitfy={jusitfy ? jusitfy : 'flex-start'}
+          >
+            {data.results.map(result => (
+              <Card key={result.id} data={result} mediaType={mediaType} />
+            ))}
+          </StyledContainer>
+          {havePagination && (
+            <Pagination
+              page={data.page}
+              maxPage={data.total_pages}
+              path={mediaType === 'movie' ? 'movies' : 'tv'}
+            />
+          )}
+        </>
       )}
-    </StyledContainer>
+    </>
   );
 };
 
@@ -31,11 +65,21 @@ const StyledContainer = styled.article`
   padding: 2rem 0;
   display: flex;
   flex-wrap: ${({ wrap }) => wrap};
+  justify-content: ${({ jusitfy }) => jusitfy};
 `;
 
+CardList.defaultProps = {
+  url: '',
+  wrap: false,
+  jusitfy: 'flex-start',
+  havePagination: false,
+};
 CardList.propTypes = {
   wrap: PropTypes.bool,
+  havePagination: PropTypes.bool,
+  jusitfy: PropTypes.string,
   title: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
+  url: PropTypes.string,
   mediaType: PropTypes.string.isRequired,
+  page: PropTypes.number,
 };
