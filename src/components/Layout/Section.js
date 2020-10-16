1a13809feef39c7fe47a9bc32ac4edd277e829c9
Piotr Glodek <pioglodek@gmail.com>
2020-10-16 16:55:23 +0200

    Move SectionTitle to Section

--- src/components/Layout/Section.js
+++ src/components/Layout/Section.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export const Section = ({ children }) => {
-  return <StyledSection>{children}</StyledSection>;
+export const Section = ({ title, children }) => {
+  return (
+    <StyledSection>
+      <StyledSectionTitle>{title}</StyledSectionTitle>
+      {children}
+    </StyledSection>
+  );
 };
 
 const StyledSection = styled.section`
   padding: 3rem 0;
 `;
+
+const StyledSectionTitle = styled.p`
+  margin: 2rem 0;
+  color: ${({ theme: { color } }) => color.white};
+  font-size: 1.6rem;
+`;
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
