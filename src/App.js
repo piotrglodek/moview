87196d5e16ac640209a34a320d0da3918b63aead
Piotr Glodek <pioglodek@gmail.com>
2020-10-16 22:05:31 +0200

    Add Main and Routes for movies and series

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Providers } from './Providers';
 import { Switch, Route } from 'react-router-dom';
 // components
-import { Navigation, Footer } from './components';
+import { Navigation, Footer, Main } from './components';
 // pages
-import { Home } from './pages';
+import { Home, Shows } from './pages';
 // temp React query dev tools
 import { ReactQueryDevtools } from 'react-query-devtools';
 
@@ -12,9 +12,21 @@ function App() {
   return (
     <Providers>
       <Navigation />
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
+      <Main>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route
+            exact
+            path='/movies/page/:pageNumber'
+            component={() => <Shows mediaType='movie' />}
+          />
+          <Route
+            exact
+            path='/tv/page/:pageNumber'
+            component={() => <Shows mediaType='tv' />}
+          />
+        </Switch>
+      </Main>
       <Footer />
       <ReactQueryDevtools />
     </Providers>
