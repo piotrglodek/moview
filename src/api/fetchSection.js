cc800b4a9cc37475fb1d94d7fedad933b9426176
Piotr Glodek <pioglodek@gmail.com>
2020-10-17 18:11:16 +0200

    Add new params to fetchSection

--- src/api/fetchSection.js
+++ src/api/fetchSection.js
@@ -1,13 +1,25 @@
 import { base_url, KEY } from './';
 
-export const fetchSection = async (name, url, page) => {
-  let fetchUrl =
-    page && name === 'movie'
-      ? `${base_url}discover/movie?api_key=${KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
-      : page && name === 'tv'
-      ? `${base_url}discover/tv?api_key=${KEY}&language=en-US&sort_by=popularity.desc&page=${page}&include_null_first_air_dates=false`
-      : url;
-  const response = await fetch(fetchUrl);
+export const fetchSection = async (
+  name,
+  page,
+  getBy,
+  discover,
+  similar,
+  id
+) => {
+  let url;
+  if (getBy) {
+    url = `${base_url}${name}/${getBy}?api_key=${KEY}&language=en-US&page=${page}`;
+  } else if (discover) {
+    url = `${base_url}${discover}/${name}?api_key=${KEY}&language=en-US&page=${page}`;
+  } else if (similar) {
+    url = `${base_url}${name}/${id}/similar?api_key=${KEY}&language=en-US&page=${page}`;
+  } else {
+    url = `${base_url}${name}?api_key=${KEY}&language=en-US&page=${page}`;
+  }
+
+  const response = await fetch(url);
   const json = await response.json();
   return json;
 };
