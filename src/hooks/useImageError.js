d958850c792484cb0df46e85bc87ba7b72b1e5db
Piotr Glodek <pioglodek@gmail.com>
2020-10-17 18:10:09 +0200

    Rename useCardImageError to useImageError

--- src/hooks/useImageError.js
+++ src/hooks/useImageError.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const useCardImageError = () => {
+export const useImageError = () => {
   const [isError, setError] = useState(false);
   const handleImageError = () => setError(true);
   return [isError, handleImageError];
